4cb681184fe6238c51b19318362cca865462ca9c
Richard Astbury <richard.astbury@gmail.com>
2014-12-10 12:09:23 +0000

    added Jester code

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using Jester
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+test("this is a description") do 
+	"hello"
+end
+
+test() do 
+	assert(1 == 1)
+	assert(2 == 2)
+end
+
+
+test() do 
+	assert(100 == 99, "100 is not 99")
+end
+
